a2d5b3148dc34109ae8dae921f4d57fa9c0145bd
mskim1023 <rlaaudtj@gist.ac.kr>
2020-04-10 19:54:56 +0900

    0409 result added

--- results/0409/run.sh
+++ results/0409/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#python3 tuning.py --max_evals=3000 --save_file=0409/Y12_lgb --method=lgb --nfold=10 --label=Y12
-python3 tuning.py --max_evals=1500 --save_file=0409/Y13_lgb --method=lgb --nfold=10 --label=Y13
-python3 tuning.py --max_evals=1500 --save_file=0409/Y15_lgb --method=lgb --nfold=10 --label=Y15
-python3 tuning.py --max_evals=3000 --save_file=0409/Y16_lgb --method=lgb --nfold=10 --label=Y16
+python3 tuning.py --max_evals=1000 --save_file=0409/Y16_lgb --method=lgb --nfold=10 --label=Y16
+python3 tuning.py --max_evals=1000 --save_file=0409/Y11_lgb --method=lgb --nfold=10 --label=Y11
+python3 tuning.py --max_evals=1000 --save_file=0409/Y17_lgb --method=lgb --nfold=10 --label=Y17
+python3 tuning.py --max_evals=1000 --save_file=0409/Y09_lgb --method=lgb --nfold=10 --label=Y09
